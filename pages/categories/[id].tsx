130f1b68e43247497dfb4a25faeaba5db8641c41
emh33 <55838653+emh33@users.noreply.github.com>
2022-04-18 17:46:33 +0000

    laga villur til að geta keyrt build
    þarf að laga typeScript!

--- pages/categories/[id].tsx
+++ pages/categories/[id].tsx
@@ -1,40 +1,33 @@
+import { GetServerSideProps, GetStaticPaths } from 'next';
+import Head from 'next/head';
 
-import Head from 'next/head'
-
-
-
-function Category({category}) {
-	return (
-		<div>
-			<Head>
-				<title>{category.title}</title>
-			</Head>
-			<h1>{category.title}</h1>
-		</div>
-		)
+function Category({ category }:any) {
+  return (
+   <div>
+    <Head>
+     <title>{category.title}</title>
+    </Head>
+    <h1>{category.title}</h1>
+   </div>
+  );
 }
 
+export const getStaticPaths: GetStaticPaths = async () => {
+  const res = await fetch('https://vef2-2022-h1-synilausn.herokuapp.com/categories');
+  const categories = await res.json();
+  // console.log(categories)
+  const paths = categories.items.map((category:any) => ({
+    params: { id: category.id.toString() },
+  }));
 
+  return { paths, fallback: false };
+};
 
+export const getStaticProps: GetServerSideProps = async ({ params }) => {
+  const id = params?.id;
+  const res = await fetch(`https://vef2-2022-h1-synilausn.herokuapp.com/categories/${id}`);
+  const category = await res.json();
+  return { props: { category } };
+};
 
-
-
-
-export async function getStaticPaths() {
-	const res = await fetch('https://vef2-2022-h1-synilausn.herokuapp.com/categories')
-	const categories = await res.json()
-	//console.log(categories)
-	const paths = categories.items.map((category) => ({
-    	params: { id: category.id.toString() },
-  	}))
-
-	return { paths, fallback: false}
-}
-
-export async function getStaticProps({ params }) {
-	const res = await fetch(`https://vef2-2022-h1-synilausn.herokuapp.com/categories/${params.id}`)
-	const category = await res.json()
-	return { props: { category } }
-}
-
-export default Category
+export default Category;
--- pages/menu.tsx
+++ pages/menu.tsx
@@ -1,10 +1,9 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import Link from 'next/link'
+import Link from 'next/link';
 import styles from '../styles/Home.module.css';
 
-const Menu: NextPage = ({categories, menu}: Props) => {
-  return(
+const Menu: NextPage = ({ categories, menu }: any) => (
 
     <div className={styles.container}>
       <Head>
@@ -14,26 +13,23 @@ const Menu: NextPage = ({categories, menu}: Props) => {
       </Head>
       <main>
         <p>Hér verður menu</p>
-        
+
           <ul>
-            {categories.items.map(function(item: any, i:number){
-              return(
-                <Link href={'/categories/'+item.id.toString()}>
-                  <a><li key={i}>{item.title}</li></a>
+            {categories.items.map((item: any, i:number) => (
+                <Link key={i} href={`/categories/${item.id.toString()}`}>
+                  <li >{item.title}</li>
                 </Link>
-              )
-            })}
+            ))}
           </ul>
       </main>
     </div>
-  )
-}
+);
 export async function getServerSideProps() {
-  const categoriesRes = await fetch('https://vef2-2022-h1-synilausn.herokuapp.com/categories')
-  const categories = await categoriesRes.json()
-  const menuRes = await fetch('https://vef2-2022-h1-synilausn.herokuapp.com/menu')
-  const menu = await menuRes.json()
-  return { props: {categories, menu}}
+  const categoriesRes = await fetch('https://vef2-2022-h1-synilausn.herokuapp.com/categories');
+  const categories = await categoriesRes.json();
+  const menuRes = await fetch('https://vef2-2022-h1-synilausn.herokuapp.com/menu');
+  const menu = await menuRes.json();
+  return { props: { categories, menu } };
 }
 
 export default Menu;
